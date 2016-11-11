e045525cdc78f6bf718af61ede95b69ea86aac64
jhartman <jhartman@sfu.ca>
2016-11-11 11:22:36 -0500

    Add enviroment varibale batch file

---
+++ env.bat
@@ -0,0 +1,12 @@
+setx           CUDA_PATH "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5"
+setx      CUDA_PATH_V7_5 "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5"
+setx            CUB_PATH D:\ThirdParty\NVIDIA\cub-1.5.5
+setx          CUDNN_PATH D:\ThirdParty\NVIDIA\cudnn-7.5
+setx  BOOST_INCLUDE_PATH D:\ThirdParty\boost_1_60_0
+setx      BOOST_LIB_PATH D:\ThirdParty\boost_1_60_0\lib64-msvc-12.0
+setx       CNTK_MKL_PATH D:\ThirdParty\CNTKCustomMKL-Windows-2
+setx     OPENCV_PATH_V31 D:\ThirdParty\opencv-3-1\build
+setx           ZLIB_PATH D:\ThirdParty\zip
+setx       PROTOBUF_PATH D:\ThirdParty\protobuf-3.1.0
+setx           SWIG_PATH D:\ThirdParty\swigwin-3.0.10
+setx      CNTK_PY34_PATH C:\Program Files\Anaconda3\envs\cntk-py35;C:\Program Files\Anaconda3\envs\cntk-py35\Library\mingw-w64\bin;C:\Program Files\Anaconda3\envs\cntk-py35\Library\usr\bin;C:\Program Files\Anaconda3\envs\cntk-py35\Library\bin;C:\Program Files\Anaconda3\envs\cntk-py35\Scripts
